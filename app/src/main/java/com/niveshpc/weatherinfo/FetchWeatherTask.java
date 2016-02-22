e7302199b9310761d897426e85752cf0d092408c
Nivesh Jain <niveshjain2012@gmail.com>
2016-02-22 12:09:25 +0530

    displaying forecast for 14 days,check it

--- app/src/main/java/com/niveshpc/weatherinfo/FetchWeatherTask.java
+++ app/src/main/java/com/niveshpc/weatherinfo/FetchWeatherTask.java
@@ -329,7 +329,7 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
             // Possible parameters are avaiable at OWM's forecast API page, at
             // http://openweathermap.org/API#forecast
             final String FORECAST_BASE_URL =
-                    "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                    "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=5e419dd169645528f464030b0ba4c461&";
             final String QUERY_PARAM = "q";
             final String FORMAT_PARAM = "mode";
             final String UNITS_PARAM = "units";
